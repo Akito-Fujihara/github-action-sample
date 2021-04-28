b573e4ae95a76f684e652ff6cee21e758c802a82
Akito-Fujihara <1998922akito@gmail.com>
2021-04-28 15:55:29 +0900

    first commit!

---
+++ hello.py
@@ -0,0 +1,5 @@
+"""関数 `hello()` を提供する"""
+
+def hello(name: str = "world") -> None:
+    """`Hello, xxx.` のメッセージを出力する"""
+    print(f"Hello, {name}.")
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_package
+
+setup(name="hello-github-actions", version="1.0", packages=find_packages())
